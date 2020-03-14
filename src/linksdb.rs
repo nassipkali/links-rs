b52e634d373228f8875fb86b6877d36b483e6102
nasipkalid <virtmem4096@gmail.com>
2020-03-14 12:39:57 -0500

    Database Opening added

---
+++ src/linksdb.rs
@@ -0,0 +1,48 @@
+extern crate memmap;
+use std::fs::OpenOptions;
+use std::path::PathBuf;
+use std::fs::File;
+use memmap::MmapMut;
+
+mod sbt_link;
+//mod sbt_methods;
+
+pub use sbt_link::Link;
+
+
+
+
+struct LinksDB<T> {
+    links: *mut Link<T>,
+    allocated_links: T,
+    reserved_links: T,
+    free_links: T,
+    first_free_link: T,
+    last_free_link: T,
+    block_size: T,
+    db_file: File,
+    mmap: MmapMut
+}
+
+impl<T> LinksDB<T> {
+    fn open(&mut self, path: PathBuf) {
+        self.db_file = OpenOptions::new()
+                       .read(true)
+                       .write(true)
+                       .create(true)
+                       .open(&path)
+                       .ok()
+                       .unwrap();
+        unsafe {
+            let mmap = MmapMut::map_mut(&self.db_file)
+                .ok()
+                .unwrap();
+            let mmap_ptr = mmap
+                .first()
+                .unwrap();
+
+            self.links = std::mem::transmute::<&u8, *mut Link<T>>(mmap_ptr);
+        }; 
+    }
+}
+
---
+++ src/linksdb/sbt_link.rs
@@ -0,0 +1,15 @@
+
+
+pub struct Link <T> {
+    source: T,
+    target: T,
+    root_as_source: T,
+    left_as_source: T,
+    right_as_source: T,
+    size_as_soucre: T,
+    root_as_target: T,
+    left_as_target: T,
+    right_as_target: T,
+    size_as_target: T
+}
+
